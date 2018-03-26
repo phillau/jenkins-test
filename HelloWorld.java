4a1be7b9ec12b1c861e8451369f200ecbd2dc4cf
liufei <2317741837@qq.com>
2018-03-26 14:59:13 +0800

    jenkins test

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.sinosoft;
+/**
+* @author 刘飞
+* @version 创建时间：2018年3月26日 下午2:46:44
+* @Description 类描述
+*/
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello jenkins!!!");
+	}
+}
